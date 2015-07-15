278e365abcb0a178ad5415c6e7de3ce42ac7eb95
Balazs Molnar <balazs_molnar@epam.com>
2015-07-15 14:45:13 +0200

    rename getList method

--- Improvement/Scripts/Controllers/ImprovementService.js
+++ Improvement/Scripts/Controllers/ImprovementService.js
@@ -1,6 +1,6 @@
 ﻿angular.module('app').service('ImprovementService', ['$http', '$q', function ($http, $q) {
 
-    this.getList = function () {
+    this.getAllImprovements = function () {
         var deferred = $q.defer();
         $http.get('/Api/Improvement/').success(function(data, status, headers, config) {
             deferred.resolve(data);
--- Improvement/Scripts/Controllers/LandingPageController.js
+++ Improvement/Scripts/Controllers/LandingPageController.js
@@ -3,7 +3,7 @@
 var LandingPageController = function ($scope, $http, ImprovementService) {
 
     $scope.helloAngular = 'I work!';
-    ImprovementService.getList().then(function (data) { $scope.movies = data; });
+    ImprovementService.getAllImprovements().then(function (data) { $scope.movies = data; });
 }
 
 angular.module('app').controller('LandingPageController', LandingPageController);
