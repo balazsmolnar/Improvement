2f3638702e0c937062fe757098e2a5e870d5016e
Balazs Molnar <balazs_molnar@epam.com>
2015-07-22 14:29:10 +0200

    Add quote at the top of the page

--- Improvement/Scripts/Controllers/LandingPageController.js
+++ Improvement/Scripts/Controllers/LandingPageController.js
@@ -2,7 +2,7 @@
 
 var LandingPageController = function ($scope, $http, ImprovementService) {
 
-    $scope.helloAngular = 'I work!';
+    $scope.quote = '“There is only one corner of the universe you can be certain of improving, and that\'s your own self.” - Aldous Huxley';
     ImprovementService.getAllImprovements().then(function (data) { $scope.movies = data; });
 }
 
