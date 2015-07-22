e216829d541078f0b2da0afdfee4bd3fd12a8f16
Balazs Molnar <balazs_molnar@epam.com>
2015-07-22 14:35:13 +0200

    fix merge error

--- Improvement/Scripts/Controllers/LandingPageController.js
+++ Improvement/Scripts/Controllers/LandingPageController.js
@@ -3,7 +3,7 @@
 var LandingPageController = function ($scope, $http, ImprovementService) {
 
     $scope.quote = '“There is only one corner of the universe you can be certain of improving, and that\'s your own self.” - Aldous Huxley';
-    ImprovementService.getAllImprovements().then(function (data) { $scope.movies = data; });
+    ImprovementService.getAllImprovements().then(function (data) { $scope.improvements = data; });
 
     $scope.submitForm = function () {
         ImprovementService.addImprovement({ title: $scope.formtitle, description: $scope.formdescription });
